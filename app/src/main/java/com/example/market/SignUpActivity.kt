663938bea60585d86eb37aa2f6fd61685384265a
walesmin <walesmin@naver.com>
2023-11-15 11:15:38 +0900

    sign_up page just link2

--- app/src/main/java/com/example/market/SignUpActivity.kt
+++ app/src/main/java/com/example/market/SignUpActivity.kt
@@ -1,13 +1,26 @@
 package com.example.market
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textview.MaterialTextView
 
 class SignUpActivity : AppCompatActivity() {
+
+    val back: MaterialTextView by lazy {
+        findViewById<MaterialTextView>(R.id.textViewLoginLink)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-    }
 
+
+        back.setOnClickListener {
+            // LoginActivity로 이동
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish() // 현재 SignUpActivity 종료
+        }
+    }
 }
